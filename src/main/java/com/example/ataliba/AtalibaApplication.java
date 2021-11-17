4711e4eb910c0063c25499835dfcddf4df41292d
cezardias <cezar.dias@gmail.com>
2021-11-17 11:25:20 -0300

    primeiro sistema

---
+++ src/main/java/com/example/ataliba/AtalibaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ataliba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtalibaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtalibaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ataliba/AtalibaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ataliba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtalibaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
